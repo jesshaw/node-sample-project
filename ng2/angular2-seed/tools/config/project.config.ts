f95b1fab609af231c235839d9413038df67c6845
mzxiao <mzxiao@Ctrip.com>
2016-08-26 16:23:05 +0800

    add ng seed test project

---
+++ ng2/angular2-seed/tools/config/project.config.ts
@@ -0,0 +1,48 @@
+import { join } from 'path';
+
+import { SeedConfig } from './seed.config';
+
+/**
+ * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
+ * below.
+ */
+export class ProjectConfig extends SeedConfig {
+
+  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+
+  constructor() {
+    super();
+    // this.APP_TITLE = 'Put name of your app here';
+
+    /* Enable typeless compiler runs (faster) between typed compiler runs. */
+    // this.TYPED_COMPILE_INTERVAL = 5;
+
+    // Add `NPM` third-party libraries to be injected/bundled.
+    this.NPM_DEPENDENCIES = [
+      ...this.NPM_DEPENDENCIES,
+      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      // {src: 'lodash/lodash.min.js', inject: 'libs'},      
+      {src: 'ng2-bootstrap/bundles/ng2-bootstrap.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true},
+    ];
+
+    this.SYSTEM_CONFIG_DEV.paths['angular2-jwt'] =
+      `${this.APP_BASE}node_modules/angular2-jwt/angular2-jwt`;
+
+    this.SYSTEM_BUILDER_CONFIG.packages['angular2-jwt'] = {
+        main: 'angular2-jwt.js',
+        defaultExtension : 'js'
+    };
+
+    // Add `local` third-party libraries to be injected/bundled.
+    this.APP_ASSETS = [
+      ...this.APP_ASSETS,
+      // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
+      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+    ];
+
+    /* Add to or override NPM module configurations: */
+    // this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
+  }
+
+}
